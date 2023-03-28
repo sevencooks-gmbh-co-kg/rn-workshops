fd1156191a679ddeac4722058a7c1b50572c1d8c
Daniel Nehring (NehrDani) <nehrdani@gmail.com>
2023-03-28 09:16:32 +0200

    add more dependencies

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
-  );
+    </GestureHandlerRootView>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +19,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
